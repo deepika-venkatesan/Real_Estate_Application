ba32d88d05045d2fed9db4e5328a51ef9a1cf394
deepika-venkatesan <deepika.venkatesanj@gmail.com>
2024-02-21 16:44:24 -0500

    Added sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -54,6 +54,16 @@ export const google = async(req, res, next) => {
             
         }
     }catch (error){
-        next(error)
+        next(error);
     }
-}
+};
+
+export const signOut = async (req, res, next) => {
+    try{
+        res.clearCookie('access_token');
+        res.status(200).json('User has been logged out!');
+
+    }catch (error) {
+        next(error);
+    }
+};
