7a1e3a4bb4b7a821065f0684b0830c4ce79fb0cc
deepika-venkatesan <deepikajvenkatesan@gmail.com>
2024-03-06 16:48:32 -0500

    Completed update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import About from './pages/About';
 import Profile from './pages/Profile';
-import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-    
+      <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute />} >
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
-  
     </BrowserRouter>
   );
 }
