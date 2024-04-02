f863867ccec80d500e8a7d76fb4937fbac787984
deepika-venkatesan <deepikajvenkatesan@gmail.com>
2024-04-02 17:58:57 -0400

    Added pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +28,8 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
